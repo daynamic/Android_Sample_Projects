459cc4da7e61fcedb7535b96023b46356c450dd8
SHARMA <AKSHAT.SHARMA@capco.com>
2018-12-04 12:48:00 +0530

    conversion of cel to fahren and using mockito as a testing frame work on android test and local JVM tests

---
+++ tempconvwithunittests/app/src/androidTest/java/com/example/atzz/temperatureconverterunittests/Utility/WriteConfigurationUtilTest.java
@@ -0,0 +1,48 @@
+package com.example.atzz.temperatureconverterunittests.Utility;
+
+import android.content.Context;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.io.FileOutputStream;
+
+import static org.junit.Assert.*;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class WriteConfigurationUtilTest {
+
+    @Rule
+    public MockitoRule rule = MockitoJUnit.rule();
+    @Mock
+    Context context;
+
+    @Mock
+    FileOutputStream fileOutputStream;
+
+
+    @Test
+    public void writeShouldWriteTwiceToFileSystem() {
+        try {
+            when(context.openFileOutput(anyString(), anyInt())).thenReturn(fileOutputStream);
+            WriteConfigurationUtil.writeConfiguration(context);
+            verify(context, times(1)).openFileOutput(anyString(), anyInt());
+            verify(fileOutputStream, atLeast(2)).write(any(byte[].class));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail();
+        }
+    }
+}
---
+++ tempconvwithunittests/app/src/test/java/com/example/atzz/temperatureconverterunittests/Utility/ConverterUtilTest.java
@@ -0,0 +1,36 @@
+package com.example.atzz.temperatureconverterunittests.Utility;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ConverterUtilTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testConvertFahrenheitToCelsius() {
+        float actual = ConverterUtil.convertCelsiusToFahrenheit(100);
+        // expected value is 212
+        float expected = 212;
+        // use this method because float is not precise
+        assertEquals("Conversion from celsius to fahrenheit failed", expected, actual, 0.001);
+    }
+
+    @Test
+    public void testConvertCelsiusToFahrenheit() {
+        float actual = ConverterUtil.convertFahrenheitToCelsius(212);
+        // expected value is 100
+        float expected = 100;
+        // use this method because float is not precise
+        assertEquals("Conversion from celsius to fahrenheit failed", expected, actual, 0.001);
+    }
+}
